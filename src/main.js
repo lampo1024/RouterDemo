21dc5af4995d5df58b08478cdadebde349552510
Rector <lampo1024@qq.com>
2018-09-29 15:24:03 +0800

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
 
 Vue.config.productionTip = false
-
+console.log(router)
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App),
+	mounted() {
+		this.$router.addRoutes([{
+			path: '/contact',
+			name: 'contact',
+			component: () =>
+				import( /* webpackChunkName: "about" */ '@/views/Contact.vue')
+		}])
+	}
 }).$mount('#app')
