787bbe9386db633c3dfe24d8982fb0087cdd0b0c
Rector <lampo1024@qq.com>
2018-09-30 17:26:22 +0800

    add menu api controller

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Menu()
+        {
+            var menu = new List<MenuJsonModel>{
+                        new MenuJsonModel{ Name = "home",Path="_home",Permissions=new []{"admim"}}
+                    };
+            return Ok(menu);
+        }
+    }
+}
---
+++ backend/Models/Menu.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+public class MenuJsonModel
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public string[] Permissions { get; set; }
+}
